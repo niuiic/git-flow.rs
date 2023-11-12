31fdb967dd81dda9b601e1e8c5a5a11be6b9a3ba
niuiic <niuiicde@gmail.com>
2023-11-12 20:15:39 +0800

    refactor: git

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,1 +1,119 @@
+use std::process::Command;
 
+use anyhow::{bail, Ok, Result};
+
+#[cfg(test)]
+mod test;
+
+pub struct Git {}
+
+impl Git {
+    pub fn has_git() -> bool {
+        let output = Command::new("git").arg("--version").output();
+        if output.is_ok() {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn switch(target_branch: &str) -> Result<()> {
+        let output = Command::new("git")
+            .args(["switch", target_branch])
+            .output()?;
+        if output.status.success() {
+            Ok(())
+        } else {
+            bail!(String::from_utf8(output.stderr).unwrap());
+        }
+    }
+
+    pub fn merge(source_branch: &str) -> Result<()> {
+        let output = Command::new("git")
+            .args(["merge", source_branch])
+            .output()?;
+        if output.status.success() {
+            Ok(())
+        } else {
+            bail!(String::from_utf8(output.stderr).unwrap());
+        }
+    }
+
+    pub fn rebase(base_branch: &str) -> Result<()> {
+        let output = Command::new("git").args(["rebase", base_branch]).output()?;
+        if output.status.success() {
+            Ok(())
+        } else {
+            bail!(String::from_utf8(output.stderr).unwrap());
+        }
+    }
+
+    pub fn cherry_pick(commits: Vec<String>) -> Result<()> {
+        let output = Command::new("git")
+            .arg("cherry-pick")
+            .args(commits)
+            .output()?;
+        if output.status.success() {
+            Ok(())
+        } else {
+            bail!(String::from_utf8(output.stderr).unwrap());
+        }
+    }
+
+    pub fn del_branch(target_branch: &str) -> Result<()> {
+        let output = Command::new("git")
+            .args(["branch", "-d", target_branch])
+            .output()?;
+        if output.status.success() {
+            Ok(())
+        } else {
+            bail!(String::from_utf8(output.stderr).unwrap());
+        }
+    }
+
+    pub fn create_branch(source_branch: &str, target_branch: &str) -> Result<()> {
+        let output = Command::new("git")
+            .args(["branch", target_branch, source_branch])
+            .output()?;
+        if output.status.success() {
+            Ok(())
+        } else {
+            bail!(String::from_utf8(output.stderr).unwrap());
+        }
+    }
+
+    /// commits on source_branch but not on target_branch
+    pub fn diff_commits(source_branch: &str, target_branch: &str) -> Result<Vec<String>> {
+        let output = Command::new("git")
+            .args([
+                "log",
+                "--format=%H",
+                &format!("{}..{}", target_branch, source_branch),
+            ])
+            .output()?;
+        if !output.status.success() {
+            bail!(String::from_utf8(output.stderr).unwrap());
+        }
+
+        let output_str = String::from_utf8(output.stdout).unwrap();
+        Ok(output_str
+            .split('\n')
+            .filter(|x| *x != "")
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>())
+    }
+
+    pub fn get_branches() -> Result<Vec<String>> {
+        let output = Command::new("git").args(["branch"]).output()?;
+        if !output.status.success() {
+            bail!(String::from_utf8(output.stderr).unwrap());
+        }
+
+        let output_str = String::from_utf8(output.stdout).unwrap();
+        Ok(output_str
+            .split('\n')
+            .filter(|x| *x != "")
+            .map(|x| x.replace("*", "").trim().to_string())
+            .collect::<Vec<String>>())
+    }
+}
---
+++ src/git/test.rs
@@ -0,0 +1,56 @@
+use super::*;
+
+#[test]
+fn has_git_t() {
+    assert_eq!(Git::has_git(), true);
+}
+
+#[test]
+fn switch_t() {
+    let result = Git::switch("undefined");
+    assert_eq!(result.is_ok(), false);
+    let result = Git::switch("main");
+    assert_eq!(result.is_ok(), true);
+}
+
+#[test]
+fn merge_t() {
+    let result = Git::merge("undefined");
+    assert_eq!(result.is_ok(), false);
+}
+
+#[test]
+fn rebase_t() {
+    let result = Git::rebase("undefined");
+    assert_eq!(result.is_ok(), false);
+}
+
+#[test]
+fn cherry_pick_t() {
+    let result = Git::cherry_pick(vec!["undefined".to_string()]);
+    assert_eq!(result.is_ok(), false);
+}
+
+#[test]
+fn del_branch_t() {
+    let result = Git::del_branch("undefined");
+    assert_eq!(result.is_ok(), false);
+}
+
+#[test]
+fn diff_commits_t() {
+    let result = Git::diff_commits("main", "test");
+    assert_eq!(result.is_ok(), false)
+}
+
+#[test]
+fn create_branch_t() {
+    let result = Git::create_branch("main", "main");
+    assert_eq!(result.is_ok(), false)
+}
+
+#[test]
+fn get_branches_t() {
+    let result = Git::get_branches().unwrap();
+    assert_eq!(result.iter().find(|x| x.as_str() == "main").is_some(), true);
+}
