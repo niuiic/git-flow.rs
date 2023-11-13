6b02ee4c540e961feab93fcce07fb4b52d5bf27c
niuiic <niuiicde@gmail.com>
2023-11-13 16:03:27 +0800

    fix: test

--- src/config/parse/test.rs
+++ src/config/parse/test.rs
@@ -5,7 +5,7 @@ use crate::config::read::read_config;
 fn name() {
     let config_list = read_config().unwrap();
     let (branch_type, branch_name) =
-        get_branch_type_name(&config_list, &vec!["feat/test".to_string()]).unwrap();
+        get_branch_type_name(&config_list, &vec!["feature/test".to_string()]).unwrap();
     assert_eq!(branch_type, "feature");
     assert_eq!(branch_name, "test");
     let (branch_type, branch_name) = get_branch_type_name(
