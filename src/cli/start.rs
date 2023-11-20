dd4c495df3ca1f1f3e678f5e37e822fa424a7f60
niuiic <niuiicde@gmail.com>
2023-11-20 11:07:08 +0800

    enhance(cli/start): branch_name should not be same as branch type

--- src/cli/start.rs
+++ src/cli/start.rs
@@ -3,31 +3,35 @@ use crate::{config::definition::Config, echo::Echo, git::Git};
 
 impl Cli {
     pub fn start(config_list: &Vec<Config>, branch_type: &str, branch_name: &str) {
+        // %% validate branch name %%
+        if config_list.iter().any(|x| x.branch_type == branch_name) {
+            Echo::error(&format!(
+                "Branch name should be different from branch types"
+            ));
+            return;
+        }
+
+        // %% find config %%
         let config = config_list
             .iter()
             .find(|x| x.branch_type == branch_type)
             .unwrap();
 
+        // %% calculate branch name %%
         let branch_name = config.branch_name.replace("{new_branch}", branch_name);
 
+        // %% get/validate branches %%
         let branches = Git::get_branches().unwrap();
-        if branches
-            .iter()
-            .find(|x| x.as_str() == config.source_branch)
-            .is_none()
-        {
+        if branches.iter().all(|x| x.as_str() != config.source_branch) {
             Echo::error(&format!("Source branch {} not found", config.source_branch));
             return;
         }
-        if branches
-            .iter()
-            .find(|x| x.as_str() == branch_name)
-            .is_some()
-        {
+        if branches.iter().any(|x| x.as_str() == branch_name) {
             Echo::error(&format!("Branch {} exists", branch_name));
             return;
         }
 
+        // %% create new branch %%
         Echo::progress(&format!("Create new branch {}", &branch_name));
         if let Err(err) = Git::create_branch(&config.source_branch, &branch_name) {
             println!();
@@ -37,6 +41,7 @@ impl Cli {
         print!("\r");
         Echo::success(&format!("Create new branch {}", &branch_name));
 
+        // %% switch to new branch %%
         Echo::progress(&format!("Switch to branch {}", &branch_name));
         if let Err(err) = Git::switch(&branch_name) {
             println!();
