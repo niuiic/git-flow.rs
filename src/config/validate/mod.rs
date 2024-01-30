ce65116dfde7fca38b15a4c6f210b2b13a8029a8
niuiic <niuiicde@gmail.com>
2024-01-30 12:39:23 +0800

    refactor(config): validate

--- src/config/validate/mod.rs
+++ src/config/validate/mod.rs
@@ -6,33 +6,32 @@ use super::definition::Config;
 #[cfg(test)]
 mod test;
 
-pub fn validate_config(config_list: &Vec<Config>) -> Result<()> {
-    has_duplicate_config(&config_list)?;
-    has_invalid_branch_name(&config_list)?;
-    target_is_valid_regex(&config_list)?;
+pub fn validate_config(config: &Config) -> Result<()> {
+    no_duplicate_config(&config)?;
+    target_is_valid_regex(&config)?;
     Ok(())
 }
 
-fn has_duplicate_config(config_list: &Vec<Config>) -> Result<()> {
-    for i in 0..config_list.len() {
-        let config = &config_list[i];
+fn no_duplicate_config(config: &Config) -> Result<()> {
+    let branch_types = &config.branch_types;
 
-        for j in 0..config_list.len() {
-            let config_2 = &config_list[j];
+    for i in 0..branch_types.len() {
+        let branch_type_i = &branch_types[i];
 
-            if i == j {
-                continue;
-            }
-            if config.branch_type == config_2.branch_type {
+        for j in 0..i {
+            let branch_type_j = &branch_types[j];
+
+            if branch_type_i.name == branch_type_j.name {
                 bail!(format!(
-                    "Invalid config: duplicate type {}",
-                    &config.branch_type
+                    "invalid config: duplicate branch type name {}",
+                    &branch_type_i.name
                 ));
             }
-            if config.branch_name == config_2.branch_name {
+
+            if branch_type_i.create == branch_type_j.create {
                 bail!(format!(
-                    "Invalid config: duplicate name {}",
-                    &config.branch_type
+                    "invalid config: duplicate branch type create {}",
+                    &branch_type_i.create
                 ));
             }
         }
@@ -41,40 +40,16 @@ fn has_duplicate_config(config_list: &Vec<Config>) -> Result<()> {
     Ok(())
 }
 
-fn has_invalid_branch_name(config_list: &Vec<Config>) -> Result<()> {
-    let regex = Regex::new(r"\{new_branch\}").unwrap();
+fn target_is_valid_regex(config: &Config) -> Result<()> {
+    for i in 0..config.branch_types.len() {
+        let branch_type = &config.branch_types[i];
 
-    for i in 0..config_list.len() {
-        let config = &config_list[i];
-
-        let matches: Vec<_> = regex.find_iter(&config.branch_name).collect();
-        if matches.len() == 0 {
-            bail!(format!(
-                "Invalid config: {} does not contain a {{new_branch}}",
-                &config.branch_name
-            ));
-        }
-        if matches.len() > 1 {
-            bail!(format!(
-                "Invalid config: {} contains more than one {{new_branch}}",
-                &config.branch_name
-            ));
-        }
-    }
-
-    Ok(())
-}
-
-fn target_is_valid_regex(config_list: &Vec<Config>) -> Result<()> {
-    for i in 0..config_list.len() {
-        let config = &config_list[i];
-
-        for j in 0..config.target_branches.len() {
-            let target = &config.target_branches[j];
+        for j in 0..branch_type.to.len() {
+            let target = &branch_type.to[j];
 
             if Regex::new(&target.name).is_err() {
                 bail!(format!(
-                    "Invalid config: target branch {} is not a valid regex",
+                    "invalid config: target branch {} is not a valid regex",
                     &target.name
                 ))
             }
